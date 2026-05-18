ec9de49ff4133717c40dc4e909602ab9ca8f2f46
Specialty-Fab <C.Combs@specialtyfabllc.com>
2026-05-18 09:14:29 -0400

    Create title_block.py

---
+++ parsers/title_block.py
@@ -0,0 +1,10 @@
+def extract_title_block(text):
+    return {
+        "part_number": "",
+        "drawing_number": "",
+        "revision": "",
+        "material": "",
+        "finish": "",
+        "units": "",
+        "scale": ""
+    }
