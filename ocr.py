1772234b7123ef27d9a3bb2409b4799510daa90a
Specialty-Fab <C.Combs@specialtyfabllc.com>
2026-05-19 06:16:21 -0400

    Fix OCR file formatting

--- ocr.py
+++ ocr.py
@@ -1,8 +1,8 @@
 import pytesseract
 from PIL import Image
 
-def run_ocr(image: Image.Image):
 
+def run_ocr(image: Image.Image):
     data = pytesseract.image_to_data(
         image,
         output_type=pytesseract.Output.DICT
@@ -11,12 +11,17 @@ def run_ocr(image: Image.Image):
     words = []
 
     for i, text in enumerate(data["text"]):
-
         if text.strip():
+            confidence = data["conf"][i]
+
+            try:
+                confidence = float(confidence)
+            except ValueError:
+                confidence = 0.0
 
             words.append({
                 "text": text,
-                "confidence": float(data["conf"][i]),
+                "confidence": confidence,
                 "x": data["left"][i],
                 "y": data["top"][i],
                 "width": data["width"][i],
